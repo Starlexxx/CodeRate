16a2ba924de75d64a9c7157bf6d4d78c03f41cfa
Starlexxx <apavlovskiy088@gmail.com>
2021-07-08 13:34:34 +0300

    tasks added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,24 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
   end
 
   def create
+    @task = Task.new(params.require(:task).permit(:title, :body))
+    @task.save
+    redirect_to @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 end
