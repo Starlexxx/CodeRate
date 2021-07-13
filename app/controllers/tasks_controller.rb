bb1464687a801de87b77b46a18b28617e8f7f4a2
Starlexxx <apavlovskiy088@gmail.com>
2021-07-13 16:09:59 +0300

    Css and bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,50 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @tasks = Task.all
   end
-  
+
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:title, :picture, :body))
-    @task.save
-    redirect_to @task
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, success: 'Задание успешно создано'
+    else
+      render :new, danger: 'Задание не создано'
+    end
   end
 
   def edit
-    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, success: 'Задание успешно обновлено'
+    else
+      render :edit, danger: 'Задание не обновлено'
+    end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path
+    redirect_to tasks_path, success: 'Задание успешно удалено'
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :body)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  validates :title, :body, presence: true
   mount_uploader :picture, PictureUploader
 end
